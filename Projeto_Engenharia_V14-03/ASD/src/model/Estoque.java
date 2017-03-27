bce0b93cc1aabf156a998889fc1bdfac19cd0abb
Matheus Archanjo <math.calazans56@gmail.com>
2017-03-27 14:33:54 -0300

    Projeto Atualizado com base na aula de 20-03
    Adicão de classes, e seus respectivos metodos e atributos.

--- Projeto_Engenharia_V14-03/ASD/src/model/Estoque.java
+++ Projeto_Engenharia_V14-03/ASD/src/model/Estoque.java
@@ -1,8 +1,14 @@
 package model;
 
+import java.util.Iterator;
 import java.util.List;
+
+import view.Observer;
+
 import java.util.LinkedList;
-public class Estoque {
+
+import controller.Relatorio;
+public class Estoque implements Subject {
 	private List observers;
 	private LinkedList produtos= new LinkedList();
 	
@@ -13,10 +19,29 @@ public class Estoque {
 		produtos.remove(p);
 	}
 	
-	public void buscarProduto(){
-		//codigo de buscar produto
+	public Produto buscarProduto(Produto p){
+		Iterator i = produtos.iterator();
+		while(i.hasNext()){
+			Produto pr = (Produto) i.next();
+			if(pr.getNome() == p.getNome() || pr.getSpec().getDescricao() == p.getSpec().getDescricao()){
+				return (Produto) pr;
+			}else{
+				System.out.println("Produto Não Encontrado !");
+			}
+		}
+		return null;
+		
 	}
 	public int getQtd(){
 		return this.produtos.size();
+	}
+	public void removeObserver(Observer o){
+		observers.remove(o);
+	}
+	public void registerObserver(Observer o){
+		observers.add(o);
+	}
+	public Relatorio gerarRelatorio(Relatorio r,Produto p ){
+		return r;
 	}
 }
