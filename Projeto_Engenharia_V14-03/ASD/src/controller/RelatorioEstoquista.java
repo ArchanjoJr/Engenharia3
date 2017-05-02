cf12cd25eaeac47ea9e821d85dc7efbb5914a131
Matheus Archanjo <math.calazans56@gmail.com>
2017-05-02 18:00:34 -0300

    Update RelatorioEstoquista.java

--- Projeto_Engenharia_V14-03/ASD/src/controller/RelatorioEstoquista.java
+++ Projeto_Engenharia_V14-03/ASD/src/controller/RelatorioEstoquista.java
@@ -6,7 +6,6 @@ import model.*;
 
 import java.util.LinkedList;
 
-//estoquista ve:id, qtd, data de armazenamento, fornecedor
 @SuppressWarnings("unused")
 public class RelatorioEstoquista implements Relatorio {
 
